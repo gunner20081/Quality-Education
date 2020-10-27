293eb18d9020a1eb42bbba54cb02790f1d859c44
kenso11 <45929994+kenso11@users.noreply.github.com>
2020-10-27 10:12:26 +0700

    parallax

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,23 @@
 let tl = gsap.timeline();
 gsap.registerPlugin(ScrollTrigger);
 
+gsap.from(".box_l, .box_r", {
+  scrollTrigger: {
+    trigger: ".trigger_1",
+    start: "50% 40%",
+    end: "100% 10%",
+    scrub: true
+  },
+  width:'30%', 
+  duration: 2
+});
+gsap.from(".box_t, .box_b", {
+  scrollTrigger: {
+        trigger: ".trigger_1",
+      start: "50% 40%",
+    end: "100% 10%",
+    scrub: true
+  },
+  height:'6%', 
+  duration: 2
+});
