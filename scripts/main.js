9f0265b033530908ac299a61b06a200790ecafb2
Terawat  Kanjanapanwong <41550582+Chokcolate@users.noreply.github.com>
2020-11-10 05:15:38 +0700

    edit

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,42 @@
-let tl = gsap.timeline();
+let tl = gsap.timeline;
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.from(".box_l, .box_r", {
+// gsap.to(".box_l, .box_r", {
+//   scrollTrigger: {
+//     trigger: ".trigger_1",
+//     start: "center center",
+//     end: "bottom top",
+//     scrub: true
+//   },
+//   width:'0%', 
+//   duration: 2
+// });
+// gsap.to(".box_t, .box_b", {
+//   scrollTrigger: {
+//     trigger: ".trigger_1",
+//     start: "center center",
+//     end: "bottom top",
+//     scrub: true
+//   },
+//   height:'0%', 
+//   duration: 2
+// });
+
+
+tl({
   scrollTrigger: {
     trigger: ".trigger_1",
-    start: "50% 40%",
-    end: "100% 10%",
-    scrub: true
-  },
-  width:'30%', 
-  duration: 2
-});
-gsap.from(".box_t, .box_b", {
-  scrollTrigger: {
-        trigger: ".trigger_1",
-      start: "50% 40%",
-    end: "100% 10%",
-    scrub: true
-  },
-  height:'6%', 
-  duration: 2
-});
+    start: "center center",
+    end: "bottom top",
+    scrub: true,
+    pin: true
+  }
+})
+.to(".box_l, .box_r", {width:'0%', duration: 2})
+.to(".box_t, .box_b", {height:'0%', duration: 2})
+.to(".box", {display:'block', bottom:'110%', ease: "none", duration: 2})
+.to(".box2", {display:'block', bottom:'70%', ease: "none", duration: 2})
+
+
+
+
